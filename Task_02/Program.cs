93f15ad2c72271190cc8ff9cbc452315172d6e21
Anna <zhigalovanna.28@gmail.com>
2022-11-27 12:13:19 +0300

    Add comparing two numbers

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,5 +1,7 @@
-﻿int a = -9;  
-int b = -3;  
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
 if (a > b) 
 { 
 Console.WriteLine($"max = {a},min = {b}"); 
